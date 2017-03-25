f8ef0101494b4338019dfe784ac8567ec36dcf35
Chinaski <emindanew@yandex.ru>
2017-03-26 01:27:30 +0800

    Created function conditions

--- deposit.c
+++ deposit.c
@@ -27,3 +27,23 @@ if (sum < 100000 ) {
     }
     return sum;
 }
+
+int conditions(int sum, int time){
+if ( sum < 1000 && time > 365 ) {
+	printf("The minimum deposit amount in 1000\nThe term does not exceed 365 days\n");
+	return 0;
+	
+    }
+    if (sum < 1000 ) {
+	printf("The minimum deposit amount in 1000\n");
+	return 0;
+	
+    }
+    if (time > 365) {
+	printf("The term does not exceed 365 days\n");
+	return 0;
+	
+    }
+	
+    return 1;
+}
