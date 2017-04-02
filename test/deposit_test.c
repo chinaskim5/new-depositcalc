a7dbc0925f16ddbf29b9e85027329133104759a2
Chinaski <emindanew@yandex.ru>
2017-04-02 14:52:49 +0300

    Created test

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -38,6 +38,26 @@ CTEST(calct, test3){
 CTEST(calct, test4){
     ASSERT_EQUAL(1120,calc(1000,241));
 }
+CTEST(calct, test5){
+    ASSERT_EQUAL(90000,calc(100000,1));
+}
+CTEST(calct, test6){
+    ASSERT_EQUAL(103000,calc(100000,31));
+}
+CTEST(calct, test7){
+    ASSERT_EQUAL(108000,calc(100000,121));
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
